984cefb90d30982250fab1d1b99e5eaaa14da5c5
Daniel Norman <daniel@2color.me>
2020-09-24 19:20:14 +0200

    Initial commit

---
+++ src/schema.js
@@ -0,0 +1,140 @@
+import { makeExecutableSchema, gql } from 'apollo-server'
+
+const typeDefs = gql`
+type User {
+  email: String!
+  id: ID!
+  name: String
+  posts: [Post!]!
+}
+
+type Post {
+  author: User
+  content: String
+  id: ID!
+  published: Boolean!
+  title: String!
+}
+
+type Query {
+  feed: [Post!]!
+  filterPosts(searchString: String): [Post!]!
+  post(where: PostWhereUniqueInput!): Post
+}
+
+type Mutation {
+  createDraft(authorEmail: String, content: String, title: String!): Post!
+  deleteOnePost(where: PostWhereUniqueInput!): Post
+  publish(id: ID): Post
+  signupUser(data: UserCreateInput!): User!
+}
+
+input PostWhereUniqueInput {
+  id: ID
+}
+
+input UserCreateInput {
+  email: String!
+  id: ID
+  name: String
+  posts: PostCreateManyWithoutPostsInput
+}
+
+input PostCreateManyWithoutPostsInput {
+  connect: [PostWhereUniqueInput!]
+  create: [PostCreateWithoutAuthorInput!]
+}
+
+input PostCreateWithoutAuthorInput {
+  content: String
+  id: ID
+  published: Boolean
+  title: String!
+}
+`
+
+/**
+ * @typedef { import("./context.js").prisma } prisma
+ */
+
+
+const resolvers = {
+  Query: {
+    /**
+     * @param {any} parent
+     * @param {any} args
+     * @param {{ prisma:prisma }} ctx
+     */
+    feed: (parent, args, ctx) => {
+      return ctx.prisma.post.findMany({
+        where: { published: true },
+      })
+    },
+    filterPosts: (parent, args, ctx) => {
+      return ctx.prisma.post.findMany({
+        where: {
+          OR: [
+            { title: { contains: args.searchString } },
+            { content: { contains: args.searchString } },
+          ],
+        },
+      })
+    },
+    post: (parent, args, ctx) => {
+      return ctx.prisma.post.findOne({
+        where: { id: Number(args.where.id) },
+      })
+    },
+  },
+  Mutation: {
+    createDraft: (parent, args, ctx) => {
+      return ctx.prisma.post.create({
+        data: {
+          title: args.title,
+          content: args.content,
+          published: false,
+          author: {
+            connect: { email: args.authorEmail },
+          },
+        },
+      })
+    },
+    deleteOnePost: (parent, args, ctx) => {
+      return ctx.prisma.post.delete({
+        where: { id: Number(args.where.id) },
+      })
+    },
+    publish: (parent, args, ctx) => {
+      return ctx.prisma.post.update({
+        where: { id: Number(args.id) },
+        data: { published: true },
+      })
+    },
+    signupUser: (parent, args, ctx) => {
+      return ctx.prisma.user.create(args)
+    },
+  },
+  User: {
+    posts: (parent, args, ctx) => {
+      return ctx.prisma.user
+        .findOne({
+          where: { id: parent.id },
+        })
+        .posts()
+    },
+  },
+  Post: {
+    author: (parent, args, ctx) => {
+      return ctx.prisma.post
+        .findOne({
+          where: { id: parent.id },
+        })
+        .author()
+    },
+  },
+}
+
+export const schema = makeExecutableSchema({
+  resolvers,
+  typeDefs,
+})
